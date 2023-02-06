47ad7d25955f00925c25c236693108bbec7fe9d0
burntraisin <rbkehan@gmail.com>
2023-02-06 06:17:31 -0500

    Check that problem set 02 passed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(stringInput, repeatNum) {
     let stringOutput = "";
+    if (repeatNum < 0) {
+        return "ERROR";
+    }
     for (let i = 0; i < repeatNum; i++) {
         stringOutput += stringInput;
     }
