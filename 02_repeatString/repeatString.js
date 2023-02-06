a7570116a6d52b568d3f7fc03bc16b671309075a
burntraisin <rbkehan@gmail.com>
2023-02-06 06:12:08 -0500

    Solve problem set 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, repeatNum) {
+    let stringOutput = "";
+    for (let i = 0; i < repeatNum; i++) {
+        stringOutput += stringInput;
+    }
+    return stringOutput;
 };
 
 // Do not edit below this line
