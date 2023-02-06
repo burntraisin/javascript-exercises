215cbbf4567fe36f3e00c8f81eba8d7d6a310ef8
burntraisin <rbkehan@gmail.com>
2023-02-06 06:31:11 -0500

    Solve problem set 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join(""); // Split returns a new array, then reverse reverses element order, then join puts it back together
 };
 
 // Do not edit below this line
