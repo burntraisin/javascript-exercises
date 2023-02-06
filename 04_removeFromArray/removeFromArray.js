5dc46af866ad0d4b7e1f75b7836690528c59108b
burntraisin <rbkehan@gmail.com>
2023-02-06 06:45:30 -0500

    Solve problem set 04 test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, argument) {
+    let finalOutput = [];
+    for (const userOutput of array) {   //run through array
+        if (userOutput != argument) {   //if the argument does not match
+            finalOutput.push(userOutput);  //return to add to array
+        }
+    }
+    return finalOutput;
 };
 
 // Do not edit below this line
