e8178250036c57f5946a16d611ae20bcb4e81d1e
burntraisin <rbkehan@gmail.com>
2023-02-08 05:56:20 -0500

    Solve problem set 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof(start) != typeof(end)) {
+        return "ERROR";
+    }
+    else if (start > end) {
+        for (i = start; i >= end; i--) {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (start > 0 && start < end) {
+        for (i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
