c7b430f3ad680a4978d7e319d3b39232fe6da770
burntraisin <rbkehan@gmail.com>
2023-02-08 06:13:58 -0500

    Solve problem set 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let dividedYearFour = year % 4;
+    let dividedYearHundred = year % 100;
+    let dividedYearFourHundred = year % 400;
 
+    if (dividedYearFour == 0) {
+        if (dividedYearHundred == 0 && dividedYearFourHundred != 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else if (dividedYearFourHundred == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
