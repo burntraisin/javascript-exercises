21577f51c5c56433aa40edfdae816afe9c7b45a2
burntraisin <rbkehan@gmail.com>
2023-03-15 06:30:54 -0400

    Solve problem set 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //make string lowercase, remove spaces, remove punctuation
+    let cleanString = string.replaceAll("\\s", "").toLowerCase().replaceAll(/[^a-z]/g, "");
+    //check if newstring equals a reversed newstring
+    let arrayString = cleanString.split("");
+    let reverseArrayString = arrayString.reverse();
+    let reverseString = reverseArrayString.join("").toString();
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
